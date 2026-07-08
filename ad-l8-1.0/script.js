120c57ebb001a7f2a5a8c8ca482979ca05b8d9bd
Mario Valero <mario102296@hotmail.com>
2026-07-08 13:20:45 -0600

    feat: adding a navbar

--- ad-l8-1.0/script.js
+++ ad-l8-1.0/script.js
@@ -2,3 +2,16 @@
   AD-14 - JavaScript para la página de contacto.
   Objetivo: mostrar una alerta Bootstrap cuando el usuario presione el botón Enviar.
 */
+window.addEventListener("DOMContentLoaded", () => {
+    fetch('Navnime.html')
+        .then(respuesta => {
+            if (!respuesta.ok) {
+                throw new Error('No se pudo cargar el menú')
+            }
+            return respuesta.text();
+        })
+        .then(data => {
+            document.getElementById('navnime').innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error))
+})

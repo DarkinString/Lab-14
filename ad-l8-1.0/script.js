64606e627c7234d84417546f569c9eacb14d891d
ElizabethGosaaa <andii.candii10@gmail.com>
2026-07-08 13:26:16 -0600

    Archivos de noticias arriba

--- ad-l8-1.0/script.js
+++ ad-l8-1.0/script.js
@@ -2,3 +2,18 @@
   AD-14 - JavaScript para la página de contacto.
   Objetivo: mostrar una alerta Bootstrap cuando el usuario presione el botón Enviar.
 */
+window.addEventListener("DOMContentLoaded", () => {
+
+    fetch("Noticias.html")
+        .then(respuesta => {
+            if (!respuesta.ok) {
+                throw new Error("No se pudo cargar Noticias.html");
+            }
+            return respuesta.text();
+        })
+        .then(data => {
+            document.getElementById("noticias").innerHTML = data;
+        })
+        .catch(error => console.error("Error:", error));
+
+});
